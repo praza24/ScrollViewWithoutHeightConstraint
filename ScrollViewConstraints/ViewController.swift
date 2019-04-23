72c5cb4e757088d5a97a32045b4a14f075a167f6
Prasath Srithar <prasathsrithar@hotmail.co.uk>
2019-04-23 20:57:46 +0100

    Scroll view example without height constraint

---
+++ ScrollViewConstraints/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //MARK: TUTORIAL LINK https://medium.freecodecamp.org/how-to-use-auto-layout-with-uiscrollview-for-ios-b94b8687a4cc
+
+}
+
